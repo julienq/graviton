90a687644536f5aaa14a38f0d6e2310ba56bfd05
Julien Quint <julien@igel.co.jp>
2013-01-30 00:34:01 +0900

    Very basic path closing and detection

--- graviton.js
+++ graviton.js
@@ -1,5 +1,48 @@
 // "use strict";
 
+// Cross product of two vectors u and v
+function cross_product(u, v) {
+  return (u.x * v.y) - (u.y * v.x);
+}
+
+// Intersect segment p, p_ with segment q, q_. Return the coordinates of the
+// intersection point if it exists, otherwise undefined
+// cf. http://stackoverflow.com/questions/563198/
+function intersect(p, p_, q, q_) {
+  var r = { x: p_.x - p.x, y: p_.y - p.y };
+  var s = { x: q_.x - q.x, y: q_.y - q.y };
+  var rs = cross_product(r, s);
+  if (rs !== 0) {
+    var qp = { x: q.x - p.x, y: q.y - p.y };
+    var t = cross_product(qp, s) / rs;
+    if (t >= 0 && t <= 1) {
+      var u = cross_product(qp, r) / rs;
+      if (u >= 0 && u <= 1) {
+        return { x: p.x + (t * r.x), y: p.y + (t * r.y) };
+      }
+    }
+  }
+}
+
+// Check whether the first segment in a polyline intersect any of the following
+// segments
+// TODO simplify the line to make segments bigger?
+// TODO remove extra segments before closing the polyline
+function intersect_polyline(points) {
+  for (var n = points.length, i = 0, p; i < n - 3; ++i) {
+    p = intersect(points[n - 1], points[n - 2], points[i], points[i + 1]);
+    if (p) {
+      /*SVG.appendChild(flexo.$line({ x1: points[n - 1].x, y1: points[n - 1].y,
+        x2: points[n - 2].x, y2: points[n - 2].y, stroke: "blue" }));
+      SVG.appendChild(flexo.$line({ x1: points[i].x, y1: points[i].y,
+        x2: points[i + 1].x, y2: points[i + 1].y, stroke: "green" }));*/
+      p.i = i;
+      return p;
+    }
+  }
+  return p;
+}
+
 var SVG = document.querySelector("svg");
 var WIDTH = SVG.viewBox.baseVal.width;
 var HEIGHT = SVG.viewBox.baseVal.height;
@@ -14,11 +57,21 @@ function clamp_svg_point(e, offset) {
 }
 
 var TRAIL = document.getElementById("trail");
-var TRAIL_TTL = 1200;
+var TRAIL_TTL = 900;
+var TRAIL_FREEZE = 250;
 
 // Update the trail by removing all points that are over TRAIL_TTL ms old
 function update_trail() {
   var now = Date.now();
+  if (TRAIL.__frozen) {
+    if (now - TRAIL.__frozen > TRAIL_FREEZE) {
+      TRAIL.__points = [];
+      TRAIL.setAttribute("fill", "none");
+      delete TRAIL.__frozen;
+    } else {
+      return;
+    }
+  }
   var n = TRAIL.__points.length;
   TRAIL.__points = TRAIL.__points.filter(function (p, i) {
     return now - p.added < TRAIL_TTL;
@@ -30,8 +83,25 @@ function update_trail() {
 
 // Add a point to the trail
 function trail(p) {
+  if (TRAIL.__frozen) {
+    return;
+  }
   p.added = Date.now();
   TRAIL.__points.push(p);
+  var p = intersect_polyline(TRAIL.__points);
+  if (p) {
+    TRAIL.setAttribute("fill", "yellow");
+    TRAIL.__frozen = Date.now();
+    var enemy = document.getElementById("enemy");
+    if (enemy) {
+      var rect = enemy.getBoundingClientRect(enemy);
+      var e = document.elementFromPoint(rect.left + rect.width / 2,
+          rect.top + rect.height / 2);
+      if (e === TRAIL) {
+        flexo.safe_remove(enemy);
+      }
+    }
+  }
 }
 
 var SPARK = document.getElementById("spark");
@@ -58,7 +128,7 @@ function update() {
 }
 
 // Move the spark to the given point.
-function move_spark(p, clear_trail) {
+function move_spark(p) {
   SPARK.__p = p;
   SPARK.setAttribute("transform", "translate({0}, {1}) rotate({2})"
       .fmt(p.x, p.y, flexo.random_int(-15, 15)));
@@ -75,6 +145,14 @@ var HANDLER = {
       this.up(e);
     }
   },
+
+  /*
+  down: function (e) {
+    var p = flexo.event_svg_point(e);
+    move_spark(p);
+    trail(p);
+  },
+  */
 
   // Start dragging. Record the offset from where the event was in the mask so
   // that the spark does not jump when it starts moving
@@ -106,10 +184,10 @@ var HANDLER = {
 
 // Initialize the game
 
-move_spark({ x: WIDTH / 2, y: HEIGHT / 2 }, true);
 TRAIL.__points = [];
+move_spark({ x: WIDTH / 2, y: HEIGHT / 2 }, true);
 
-MASK.addEventListener("mousedown", HANDLER, false);
+SVG.addEventListener("mousedown", HANDLER, false);
 document.addEventListener("mousemove", HANDLER, false);
 document.addEventListener("mouseup", HANDLER, false);
 MASK.addEventListener("touchstart", HANDLER, false);
